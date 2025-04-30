9ed4773bcb6f23bb2764bdc4c6bbd8073ffa67c0
Mon Nom <votreemail@votreemail.com>
2025-04-30 12:59:55 +0100

    3 commit

---
+++ src/main/java/org/example/partie1/User.java
@@ -0,0 +1,16 @@
+package org.example.partie1;
+
+
+
+public class User {
+    private long id;
+    private String name;
+    private String email;
+
+    public User(long id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+}
